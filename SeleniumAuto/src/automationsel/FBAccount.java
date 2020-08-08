1695c4271d192791bdd15389c837fcbd8454e992
Ashish <Ashish@LAPTOP-N84L60DU>
2020-08-08 18:22:33 +0530

    Adding selenium project

---
+++ SeleniumAuto/src/automationsel/FBAccount.java
@@ -0,0 +1,39 @@
+package automationsel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FBAccount {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        
+        driver.manage().window().maximize();
+        
+        
+       WebElement username = driver.findElement(By.name("firstname"));
+       username.sendKeys("Deepti");
+       
+  
+       
+       WebElement lastname = driver.findElement(By.name("lastname"));
+       lastname.sendKeys("Verma");
+       
+       WebElement mobileNo = driver.findElement(By.name("reg_email__"));
+       mobileNo.sendKeys("9999999999");
+       
+       WebElement newpasswd = driver.findElement(By.cssSelector("input#password_step_input"));
+       newpasswd.sendKeys("123@ABc");
+       
+       WebElement gender = driver.findElement(By.cssSelector("input._8esa"));
+       gender.click();
+        
+       WebElement submit = driver.findElement(By.name("websubmit"));
+       submit.click();
+	}
+
+}
